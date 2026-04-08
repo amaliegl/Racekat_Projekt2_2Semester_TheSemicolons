94d66cf1251318de7977299ff8062c82d7907f15
amali <amaliegp@gmail.com>
2026-04-08 13:22:36 +0200

    Initial commit

---
+++ src/main/java/org/example/racekat_projekt2_2semester_thesemicolons/RacekatProjekt22SemesterTheSemicolonsApplication.java
@@ -0,0 +1,13 @@
+package org.example.racekat_projekt2_2semester_thesemicolons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacekatProjekt22SemesterTheSemicolonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacekatProjekt22SemesterTheSemicolonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/racekat_projekt2_2semester_thesemicolons/RacekatProjekt22SemesterTheSemicolonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.racekat_projekt2_2semester_thesemicolons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacekatProjekt22SemesterTheSemicolonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
